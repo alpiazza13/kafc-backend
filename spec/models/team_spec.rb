a0eacfc23a91494d1fffaaaea5b60afb7f7b7bc1
Alex Piazza <alexpiazza@alexs-mbp-3.stu-wireless.wesleyan.edu>
2019-03-30 19:14:45 -0400

    [rails-10]adds team specs

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe 'model structure' do
+      it 'has its columns' do
+        expect(Team.column_names.include?('name')).to be true
+        expect(Team.column_names.include?('color')).to be true
+        expect(Team.column_names.include?('id')).to be true
+      end
+    end
+
+    describe 'validation' do
+      it 'requires a name' do
+        new_team = Team.new
+        expect(new_team.valid?).to be false
+
+        new_team.name = 'Chelsea'
+        expect(new_team.valid?).to be true
+      end
+    end
 end
