8654e24a37727608729ad650206eb989e620e22f
Alex Piazza <alexpiazza@alexs-mbp-3.stu-wireless.wesleyan.edu>
2019-04-21 14:46:49 -0400

    [rails-13] Adds teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'teams/index'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:index]
---
+++ spec/fabricators/team_fabricator.rb
@@ -0,0 +1,5 @@
+Fabricator(:team) do
+  name { Faker::Team.name.sub("'", '') }
+  color { Faker::Color.color_name.sub("'", '') }
+
+end
