e9a28360a66d98a319dda4fd3b34a3624f8fa292
Chris Piazza <piazza.chris@gmail.com>
2019-06-25 22:01:44 -0400

    adds route and fixes misspelled controller

--- app/controllers/api/v1/players_conroller.rb
+++
@@ -1,5 +0,0 @@
-class Api::V1::PlayersController < ApplicationController
-    def index
-        @players = Player.all.first(25)
-    end
-  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:index]
       resources :teams, only: [:index]
+      resources :players, only: [:index]
     end
   end
 end
