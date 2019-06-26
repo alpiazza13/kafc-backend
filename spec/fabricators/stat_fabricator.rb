6ff4ba5ac229beeac24b62b6ee481643859201d0
Alex Piazza <alexpiazza@Alexs-MBP-3.home>
2019-06-26 16:11:19 -0400

    generates some stats, easier fabricator

--- spec/fabricators/stat_fabricator.rb
+++ spec/fabricators/stat_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:stat) do
   # stat_name "MyString"
   # points    1
   list_stats = ["Goal", "Assist", "Tackle", "Dribble", "Save", "Yellow Card", "Red Card", "Interception"]
-  stat_name { list_stats[ Faker::Number.between(from = 0,to = list_stats.length-1) ] }
-  points {Faker::Number.between(from=1,to=10)} 
+  stat_name { list_stats.sample }
+  points { Faker::Number.between(from=1,to=10) }
 end
