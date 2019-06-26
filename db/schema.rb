84e93e9f0a0d53f76bbd25c7a1cb3d9b4ee59408
Alex Piazza <alexpiazza@Alexs-MacBook-Pro-3.local>
2019-06-25 22:37:47 -0400

    adds team id column to players model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_230224) do
+ActiveRecord::Schema.define(version: 2019_06_26_023307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "players", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "team_id"
   end
 
   create_table "teams", force: :cascade do |t|
