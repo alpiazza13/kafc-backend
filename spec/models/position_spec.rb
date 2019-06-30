4b5baf34cbd97d1a255042e13b6c722aa685ff50
kkkoech <kkoech@wesleyan.edu>
2019-06-29 20:16:57 -0400

    change player_position to name

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 RSpec.describe Position, type: :model do
   describe 'model structure' do
     it 'has its columns' do
-      expect(Position.column_names.include?('player_position')).to be true
+      expect(Position.column_names.include?('name')).to be true
     end
   end
 
   describe 'validation' do
-    it 'requires a player_position' do
+    it 'requires a name' do
       new_position = Position.new
       expect(new_position.valid?).to be false
 
