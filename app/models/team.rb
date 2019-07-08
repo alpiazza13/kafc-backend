f2293f67bb65a142b0f508370efceeeff02b0c0f
Alex Piazza <alexpiazza@Alexs-MBP-3.home>
2019-07-07 23:49:38 -0400

    done with relationships

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,8 @@
 class Team < ApplicationRecord
     validates :name, presence: true
-    # has_many :players
-    # has_many :matches
-    # has_many :player_stats, through: :matches
-    # has_many :stats, through: :player_stats
-    # not sure about the two commented lines
+    has_many :players
+    has_many :matches
+    has_many :player_stats, through: :matches
+    has_many :stats, through: :player_stats
+    # not sure about the above two lines
+end
