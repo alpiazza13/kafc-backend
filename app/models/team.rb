fc85e2c7daa963e5b347daffaae43a82d2e7a6fe
Alex Piazza <alexpiazza@Alexs-MBP-3.home>
2019-07-08 21:38:24 -0400

    should be done now

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,12 @@
 class Team < ApplicationRecord
     validates :name, presence: true
     has_many :players
-    has_many :matches
-    has_many :player_stats, through: :matches
+
+    def matches
+        Match.where("home_team_id = ? or away_team_id = ?", id, id)
+    end
+
+    has_many :player_stats, through: :players
     has_many :stats, through: :player_stats
-    # not sure about the above three lines. in fact, the matches relationship for teams doesn't work,
-    # nor does the player_stats or stats of course. it seems to look for a team_id column in matches model when i 
-    # call Team.first.matches, which doesn't exist (only home_team_id and away_team_id exist). Not sure how to fix
+
 end
