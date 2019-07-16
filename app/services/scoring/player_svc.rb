407227027d54d9d6651dfe9a4cad9ddaa5ea38a7
Alex Piazza <alexpiazza@Alexs-MBP-3.home>
2019-07-15 22:16:48 -0400

    not much

--- app/services/scoring/player_svc.rb
+++ app/services/scoring/player_svc.rb
@@ -1,17 +1,7 @@
 class Scoring::PlayerSvc
-
     #calculates the points earned by player in match
     def self.call(player, match)
-
-        #list of all stat_ids for player from match
         only_stats = PlayerStat.where(match_id: match.id, player_id: player.id).pluck(:stat_id)
-
-        #list of the point value for each stat for plaeyr in match
         Stat.where(id: only_stats).sum(:points)
-        # only_stats.map {|id|   Stat.find(id).points}
-
-        #adds it up
-        # stat_values.reduce { |tot, pt| tot + pt}
-
     end
 end
