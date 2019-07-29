141a400a6a589af0d49f62bd1e833925c3d0455f
Alex Piazza <alexpiazza@Alexs-MacBook-Pro-3.local>
2019-07-29 01:10:07 -0400

    reversible

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_042421) do
+ActiveRecord::Schema.define(version: 2019_07_26_063735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,6 @@ ActiveRecord::Schema.define(version: 2019_07_28_042421) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "team_id"
-    t.string "position"
-    t.string "string"
     t.integer "position_id"
   end
 
