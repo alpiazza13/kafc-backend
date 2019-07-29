4f6fba5c77550cf6d70174e19fd42a8b4dd46a66
kkkoech <kkoech@wesleyan.edu>
2019-07-29 00:39:12 -0400

    set up devise

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,5 +1,6 @@
 Fabricator(:user) do
   email { Faker::Internet.email }
+  password { Faker::Internet.password.sub("'", '') }
   first_name { Faker::Name.first_name.sub("'", '') }
   last_name { Faker::Name.last_name.sub("'", '') }
 end
